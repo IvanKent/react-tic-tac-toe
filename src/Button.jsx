b4f6871f643cfded9bc07e8c05f21cec756524bf
IvanKent <ivankentmalate.landicho@bicol-u.edu.ph>
2024-01-24 16:43:03 +0800

    created the tictactoe container

---
+++ src/Button.jsx
@@ -0,0 +1,22 @@
+import './tiktactoe.css'
+export default function Button() {
+    return (
+        <div className="boxContainer">
+            <div className="boxRowContainer">
+                <button>X</button>
+                <button>X</button>
+                <button>X</button>
+            </div>
+            <div className="boxRowContainer">
+                <button>X</button>
+                <button>X</button>
+                <button>X</button>
+            </div>
+            <div className="boxRowContainer">
+                <button>X</button>
+                <button>X</button>
+                <button>X</button>
+            </div>
+        </div>
+    )
+}
