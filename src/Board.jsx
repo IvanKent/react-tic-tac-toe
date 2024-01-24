dd4486e41959eb6deb1b9d1fb1a23e8cd348da7b
IvanKent <ivankentmalate.landicho@bicol-u.edu.ph>
2024-01-24 17:32:43 +0800

    moved the useState in the board

--- src/Board.jsx
+++ src/Board.jsx
@@ -1,22 +1,31 @@
 import './tiktactoe.css'
 import Square from './Square'
+import { useState } from 'react'
 export default function Button() {
+    const [squares, setSquares] = useState(Array(9).fill(null))
+
+    const handleClick = (i) => {
+        const nextSquares = squares.slice();
+        nextSquares[i] = "X";
+        console.log(`${i}: ${nextSquares[i]}`)
+        setSquares(nextSquares)
+    }
     return (
         <div className="boxContainer">
             <div className="boxRowContainer">
-                <Square/>
-                <Square/>
-                <Square/>
+                <Square value={squares[0]} handleClick={() => (handleClick(0))}/>
+                <Square value={squares[1]} handleClick={() => (handleClick(1))}/>
+                <Square value={squares[2]} handleClick={() => (handleClick(2))}/>
             </div>
             <div className="boxRowContainer">
-                <Square/>
-                <Square/>
-                <Square/>
+                <Square value={squares[3]} handleClick={() => (handleClick(3))}/>
+                <Square value={squares[4]} handleClick={() => (handleClick(4))}/>
+                <Square value={squares[5]} handleClick={() => (handleClick(5))}/>
             </div>
             <div className="boxRowContainer">
-                <Square/>
-                <Square/>
-                <Square/>
+                <Square value={squares[6]} handleClick={() => (handleClick(6))}/>
+                <Square value={squares[7]} handleClick={() => (handleClick(7))}/>
+                <Square value={squares[8]} handleClick={() => (handleClick(8))}/>
             </div>
         </div>
     )
--- src/Square.jsx
+++ src/Square.jsx
@@ -1,12 +1,5 @@
-import { useState } from "react"
-export default function Square() {
-    const [value, setValue] = useState(null)
-    const handleClick = () => {
-        setValue('X')
-    }
+export default function Square({squares = 0, handleClick}) {
     return (
-        <>
-            <button onClick={handleClick}>{value}</button>
-        </>
+        <button onClick={handleClick}>{squares}</button>
     )
 }
