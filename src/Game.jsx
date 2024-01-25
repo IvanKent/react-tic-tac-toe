4f2c986c8e91fd3231dfe7ea0ab9096df1ca68a6
IvanKent <ivankentmalate.landicho@bicol-u.edu.ph>
2024-01-25 10:38:00 +0800

    refactor xIsNext

--- src/Game.jsx
+++ src/Game.jsx
@@ -2,16 +2,15 @@ import Board from "./Board"
 import './tiktactoe.css'
 import { useState } from "react"
 export default function Game(){
-    const [xIsNext, setXIsNext] = useState(true);
     const [history, setHistory] = useState([Array(9).fill(null)])
     const [currentMove, setCurrentMove] = useState(0);
+    const xIsNext = currentMove % 2 === 0;
     const currentSquares = history[currentMove]
 
     const handlePlay = (nextSquares) => {
       const nextHistory = [...history.slice(0, currentMove +1), nextSquares];
         setHistory(nextHistory);
         setCurrentMove(nextHistory.length - 1);
-        setXIsNext(!xIsNext)
     }
 
     const moves = history.map((squares, move) => {
@@ -30,7 +29,6 @@ export default function Game(){
 
     const jumpTo = (nextMove) => {
       setCurrentMove(nextMove);
-      setXIsNext(nextMove % 2 === 0);
     }
     return (
         <div className="gameContainer">
